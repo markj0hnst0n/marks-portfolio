e188d4b9176e7e676b6704cbf1cc0d590b4cb78e
Mark Johnston <markjohnston@Marks-MacBook-Pro-2.local>
2021-07-08 12:29:37 +0100

    wired up about and contact links correctly

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about/', views.index, name='about'),
-    path('contact/', views.index, name='contact'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
